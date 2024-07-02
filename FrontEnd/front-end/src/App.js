ea47e32334712f67678463ca7df4f6e2cb3cab5b
thiyari <manikanth578@gmail.com>
2024-07-01 22:27:19 -0700

    commit

--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -3,63 +3,11 @@ import Home from './Components/Home';
 import Register from './Components/Register';
 import Login from './Components/Login';
 
-import './App.css';
-import { PiUploadSimpleBold } from "react-icons/pi";
-import {useState, useEffect} from 'react';
+
 
 
 function App() {
 
-  const [img,setImg] = useState("")
-  const [allImage,setAllImage] = useState([])
-
-  const imagebased64 = async (file)=>{
-    const reader = new FileReader()
-    await reader.readAsDataURL(file)
-    const data = new Promise((resolve,reject)=>{
-      reader.onload = () => resolve(reader.result)
-      reader.onerror = (err) => reject(err)
-    })
-    return data
-  }
-
-  const handleUploadImage = async(e)=>{
-    const file = e.target.files[0]
-    const image = await imagebased64(file)
-    console.log(image)
-    setImg(image)
-  }
-
-  const fetchImage = async()=>{
-    const res = await fetch("http://localhost:8000/")
-    const data = await res.json()
-    setAllImage(data.data)
-  }
-
-  const handleSubmit = async(e)=>{
-    e.preventDefault()
-    if(img){
-      const res = await fetch("http://localhost:8000/user/upload",{
-        method: "POST",
-        headers:{
-          "content-type":"application/json"
-        },
-        body: JSON.stringify({img:img})
-      })
-      const data = await res.json()
-      console.log(data)
-      if(data.status){
-        alert(data.message)
-        setImg('')
-        fetchImage()
-      }
-    }
-
-  }
-
-  useEffect(()=>{
-    fetchImage()
-  },[])
 
   return (
     <div>
@@ -71,26 +19,6 @@ function App() {
 
         </Routes>
       </BrowserRouter>
-      <div className='imageContainer'>
-        <form>
-          <label htmlFor='uploadImage'>
-            <div className='uploadBox'>
-              <input type='file' id='uploadImage' onChange={handleUploadImage}/>
-              { img ? <img src={img} alt=''/>:<PiUploadSimpleBold/>}
-            </div>
-          </label>
-          <div className='btn'>
-            <button onClick={handleSubmit}>upload</button>
-          </div>
-        </form>
-        <div className='allimage'>
-          {
-            allImage.map(e1 =>{
-              return <img src={e1.image} width={"250px"} height={"180px"} alt=''/>
-            })
-          }
-        </div>
-      </div>
     </div>
   );
 }
--- FrontEnd/front-end/src/Components/Register.jsx
+++ FrontEnd/front-end/src/Components/Register.jsx
@@ -1,5 +1,8 @@
 import axios from "axios";
-import {  useState } from "react";
+
+import '../App.css';
+import { PiUploadSimpleBold } from "react-icons/pi";
+import {useState, useEffect} from 'react';
 
 function Register() {
 
@@ -25,6 +28,60 @@ function Register() {
           alert(err);
         }
       }
+
+
+      const [img,setImg] = useState("")
+      const [allImage,setAllImage] = useState([])
+    
+      const imagebased64 = async (file)=>{
+        const reader = new FileReader()
+        await reader.readAsDataURL(file)
+        const data = new Promise((resolve,reject)=>{
+          reader.onload = () => resolve(reader.result)
+          reader.onerror = (err) => reject(err)
+        })
+        return data
+      }
+    
+      const handleUploadImage = async(e)=>{
+        const file = e.target.files[0]
+        const image = await imagebased64(file)
+        console.log(image)
+        setImg(image)
+      }
+    
+      const fetchImage = async()=>{
+        const res = await fetch("http://localhost:8000/")
+        const data = await res.json()
+        setAllImage(data.data)
+      }
+    
+      const handleSubmit = async(e)=>{
+        e.preventDefault()
+        if(img){
+          const res = await fetch("http://localhost:8000/user/upload",{
+            method: "POST",
+            headers:{
+              "content-type":"application/json"
+            },
+            body: JSON.stringify({img:img})
+          })
+          const data = await res.json()
+          console.log(data)
+          if(data.status){
+            alert(data.message)
+            setImg('')
+            fetchImage()
+          }
+        }
+    
+      }
+    
+      useEffect(()=>{
+        fetchImage()
+      },[])
+    
+
 
     return (
      <div>
@@ -85,6 +142,26 @@ function Register() {
             />
           </div>
 
+          <div className='imageContainer'>
+        <form>
+          <label htmlFor='uploadImage'>
+            <div className='uploadBox'>
+              <input type='file' id='uploadImage' onChange={handleUploadImage}/>
+              { img ? <img src={img} alt=''/>:<PiUploadSimpleBold/>}
+            </div>
+          </label>
+          <div className='btn'>
+            <button onClick={handleSubmit}>upload</button>
+          </div>
+        </form>
+        <div className='allimage'>
+          {
+            allImage.map(e1 =>{
+              return <img src={e1.image} width={"250px"} height={"180px"} alt=''/>
+            })
+          }
+        </div>
+      </div>
 
     
         <button type="submit" className="btn btn-primary mt-4" onClick={save} >Save</button>
