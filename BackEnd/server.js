44b28fddd439191cd20a9d470036e3064e352ef8
thiyari <manikanth578@gmail.com>
2024-07-06 15:06:34 +0530

    Server Update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,7 +7,11 @@ var routes = require('./route/routes');
 const cors = require('cors');
 const userModel = require('./src/users/userModel');
 
-app.use(cors());
+app.use(cors(
+    {   origin:'http://localhost:3000', 
+        credentials:true,            //access-control-allow-credentials:true
+        optionSuccessStatus:200,}
+));
 app.use(express.json({limit:"10mb"}))
 
 
