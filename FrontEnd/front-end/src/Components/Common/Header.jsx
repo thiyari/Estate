35171ef989f47028386414ba49873cf5abbf8ca7
thiyari <manikanth578@gmail.com>
2024-07-14 13:08:18 +0530

    update

--- FrontEnd/front-end/src/Components/Common/Header.jsx
+++ FrontEnd/front-end/src/Components/Common/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import { SlHome } from "react-icons/sl";
+
 function Header(){
 
     return(
         <React.Fragment>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
-    <a className="navbar-brand" href="#a">Estate</a>
+    <a className="navbar-brand" href="/"><SlHome /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#b">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#c">Plots</a>
+          <a className="nav-link"  href="#c">Plots</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#c">Flats</a>
@@ -23,22 +22,22 @@ function Header(){
         <li className="nav-item">
           <a className="nav-link" href="#c">Commercial</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#d" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            My Account
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#e">Profile</a></li>
-            <li><a className="dropdown-item" href="#f">Change Password</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#g">Logout</a></li>
-          </ul>
+        <li className="nav-item">
+          <a className="nav-link" href="#s">Search</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="../Register">Register</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="../Register">Contact Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="../Register">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="../Login">Login</a>
         </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
