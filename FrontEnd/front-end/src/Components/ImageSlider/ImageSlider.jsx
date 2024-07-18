1c64d1a3b45ae479a093895c0a063681403be14c
thiyari <manikanth578@gmail.com>
2024-07-18 23:16:01 +0530

    commit

---
+++ FrontEnd/front-end/src/Components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import './ImageSlider.css';
+import axios from 'axios';
+function ImageSlider(props) {
+  
+  const imgs=[
+    {id:0,value:"https://wallpaperaccess.com/full/2637581.jpg"},
+    {id:1,value:"https://source.unsplash.com/user/c_v_r/1900x800"},
+    {id:2,value:"https://source.unsplash.com/user/c_v_r/100x100"},
+  ]
+  const [wordData,setWordData]=useState(imgs[0])
+  const [val,setVal] = useState(0)
+  const [Images,setImages] = useState([])
+
+  useEffect(()=>{
+    axios.get(`http://localhost:8000/api/profile/${props.Id}`)
+        .then(res => {
+        if(res.data.status){
+            const profile_doc = res.data.profile          
+            setImages(profile_doc.data[0].images)
+        } 
+        })
+        .catch(err => console.log(err))
+    },[props])
+
+  console.log(Images)
+
+  const handleClick=(index)=>{
+    console.log(index)
+    setVal(index)
+    const wordSlider=imgs[index];
+    setWordData(wordSlider)
+  }
+  const handleNext = ()=>{
+    let index = val < imgs.length -1 ? val +1 : val;
+    setVal(index)
+    const wordSlider=imgs[index];
+    setWordData(wordSlider)
+  }
+  const handlePrevious = ()=>{
+    let index = val <= imgs.length -1 && val > 0? val - 1 : val;
+    setVal(index)
+    const wordSlider=imgs[index];
+    setWordData(wordSlider)
+  }
+  return (
+    <div className="main">
+      <button className='btns' onClick={handlePrevious}>P</button>
+      <img alt="" src={wordData.value} height="300" width="500" /> 
+      <button className='btns' onClick={handleNext}>N</button>
+      <div className='flex_row'>
+        {imgs.map((data,i)=>
+        <div className="thumbnail" key={i} >
+          <img alt="" className={wordData.id===i?"clicked":""} src={data.value} onClick={()=>handleClick(i)} height="70" width="100" />
+        </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ImageSlider;
