7bc12258e0ef6cab875ec6ff7863a880c247379a
thiyari <manikanth578@gmail.com>
2024-07-18 23:57:15 +0530

    update

--- FrontEnd/front-end/src/Components/ImageSlider/ImageSlider.jsx
+++ FrontEnd/front-end/src/Components/ImageSlider/ImageSlider.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './ImageSlider.css';
 import axios from 'axios';
 function ImageSlider(props) {
-  
+  /*
   const imgs=[
     {id:0,value:"https://wallpaperaccess.com/full/2637581.jpg"},
     {id:1,value:"https://png.pngtree.com/thumb_back/fw800/background/20230817/pngtree-lotus-flower-jpg-pink-lotus-flower-image_13023952.jpg"},
     {id:2,value:"https://png.pngtree.com/thumb_back/fh260/background/20230519/pngtree-landscape-jpg-wallpapers-free-download-image_2573540.jpg"},
-  ]
-  const [wordData,setWordData]=useState(imgs[0])
+  ]*/
+  
   const [Images,setImages] = useState([])
 
   useEffect(()=>{
@@ -22,21 +22,21 @@ function ImageSlider(props) {
         .catch(err => console.log(err))
     },[props])
 
-  console.log(Images)
+  const [wordData,setWordData]=useState(Images)
 
   const handleClick=(index)=>{
     console.log(index)
-    const wordSlider=imgs[index];
+    const wordSlider=Images[index];
     setWordData(wordSlider)
   }
 
   return (
     <div className="main">
-      <img alt="" src={wordData.value} height="300" width="500" /> 
+      <img alt="" src={wordData} height="300" width="500" /> 
       <div className='flex_row'>
-        {imgs.map((data,i)=>
+        {Images.map((data,i)=>
         <div className="thumbnail" key={i} >
-          <img alt="" className={wordData.id===i?"clicked":""} src={data.value} onClick={()=>handleClick(i)} height="70" width="100" />
+          <img alt="" className={wordData.id===i?"clicked":""} src={data} onClick={()=>handleClick(i)} height="70" width="100" />
         </div>
         )}
       </div>
