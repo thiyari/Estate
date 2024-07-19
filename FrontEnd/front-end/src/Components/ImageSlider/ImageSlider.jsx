8a203d6be2078c90331f12735e9e82ce39318a9f
thiyari <manikanth578@gmail.com>
2024-07-19 14:17:10 +0530

    adjustments done

--- FrontEnd/front-end/src/Components/ImageSlider/ImageSlider.jsx
+++ FrontEnd/front-end/src/Components/ImageSlider/ImageSlider.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-//import './ImageSlider.css'
 
 function ImageSlider(props) {
 
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
   const [Images,setImages] = useState([])
+  const [uploadToggle,setUploadToggle] = useState(false)
 
   useEffect(()=>{
     axios.get(`http://localhost:8000/api/profile/${props.Id}`)
@@ -33,13 +33,33 @@ function ImageSlider(props) {
         console.log(currentPhotoIndex)
       };
 
+    const imagebased64 = async (file)=>{
+        const reader = new FileReader()
+        await reader.readAsDataURL(file)
+        const data = new Promise((resolve,reject)=>{
+          reader.onload = () => resolve(reader.result)
+          reader.onerror = (err) => reject(err)
+        })
+        return data
+      }
+
+    const handleUploadImage = async (e) => {
+        e.preventDefault()
+          let images = []
+          for (let i = 0; i < e.target.files.length; i++) {
+             images.push(await imagebased64(e.target.files[i]))
+          }
+        setUploadToggle(true)  
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUploadToggle(false)
+    } 
+
     const handleDelete = (e) => {
         e.preventDefault()
         console.log(currentPhotoIndex)
-      };
-
-    const handleUploadImage = (e) => {
-        e.preventDefault()
       };
 
   return (
@@ -62,19 +82,22 @@ function ImageSlider(props) {
     <tr>
       <th scope="col">
         <td><button onClick={handlePrevClick}><i className="fa fa-angle-double-left" style={{fontSize:"18px"}}></i></button></td>
-        <td><button onClick={handleNextClick}><i className="fa fa-angle-double-right" style={{fontSize:"18px"}}></i></button></td>
-        <td><p style={{fontWeight:"lighter"}}>Picture [{currentPhotoIndex+1}/{Images.length}]</p></td>
-        <td>
+        <td><p style={{fontWeight:"lighter"}}>[{currentPhotoIndex+1}/{Images.length}]</p></td>
+        <td><button onClick={handleNextClick}><i className="fa fa-angle-double-right" style={{fontSize:"18px"}}></i></button></td>        
+        <td>{ uploadToggle ? 
+          <button type="submit" style={{paddingLeft: 80, paddingRight: 80, width: 25}} onClick={handleSubmit}><i class="fa fa-check" aria-hidden="true"></i></button>
+        :
           <div>
             <label htmlFor='uploadImage'>
-              <div style={{paddingLeft:60, cursor: 'pointer'}}>
-                <input  type='file' multiple accept="image/*" id='uploadImage' onChange={handleUploadImage}/>
+              <div style={{paddingLeft: 80,  paddingRight: 80, cursor: 'pointer'}}>
+                <input type='file' multiple accept="image/*" id='uploadImage' onChange={handleUploadImage}/>
                 <i className="fa fa-upload" aria-hidden="true"></i>
               </div>
             </label>
           </div>
+          }
         </td>
-        <td><button style={{paddingLeft:80}} onClick={handleDelete}><i className="fas fa-trash-alt"></i></button></td>
+        <td><button style={{width: 25}} onClick={handleDelete}><i className="fas fa-trash-alt"></i></button></td>
       </th>
     </tr>
   </tbody>
