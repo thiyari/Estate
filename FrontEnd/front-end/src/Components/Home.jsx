bd048fddd0ea0674d6ecf22641f9ffc74ab4e070
thiyari <manikanth578@gmail.com>
2024-07-22 20:15:07 +0530

    update

--- FrontEnd/front-end/src/Components/Home.jsx
+++ FrontEnd/front-end/src/Components/Home.jsx
@@ -25,55 +25,43 @@ function Home(props) {
 <div className="row">
 <div className="col-md-1"></div>
     <div className="col-md-10">
-        <div className="row">
-        
-        <div className="col-md-6">
-            <div className="container card mt-4" style={{width: "35rem"}}>
-            <img className="card-img-top mt-3" src="https://images.pexels.com/photos/1669799/pexels-photo-1669799.jpeg?cs=srgb&dl=pexels-fotoaibe-1669799.jpg&fm=jpg" alt="Card images cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#go" className="btn btn-primary">Go</a>
+        <div className='container mt-2'>
+            <div className="row">  
+
+                <div className="col-sm-6">
+                    <div className="card mt-3">
+                    <div className="card-body">
+                        <img className='img-fluid' alt="img" src="https://img.freepik.com/free-psd/blank-wall-psd-japandi-living-room-interior_53876-109284.jpg" width="100%"/>
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#go" className="btn btn-primary">Go</a>
+                    </div>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-        <div className="col-md-6">
-            <div className="container card mt-4" style={{width: "35rem"}}>
-            <img className="card-img-top mt-3" src="https://img.freepik.com/free-psd/blank-wall-psd-japandi-living-room-interior_53876-109284.jpg" alt="Card images cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#go" className="btn btn-primary">Go</a>
+                <div className="col-sm-6">
+                    <div className="card mt-3">
+                    <div className="card-body">
+                    <   img className='img-fluid' alt="img" src="https://img.freepik.com/free-psd/blank-wall-psd-japandi-living-room-interior_53876-109284.jpg" width="100%"/>
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#go" className="btn btn-primary">Go</a>
+                    </div>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-        <div className="col-md-6">
-            <div className="container card mt-4" style={{width: "35rem"}}>
-            <img className="card-img-top mt-3" src="https://img.freepik.com/free-psd/blank-wall-psd-japandi-living-room-interior_53876-109284.jpg" alt="Card images cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#go" className="btn btn-primary">Go</a>
+                <div className="col-sm-6">
+                    <div className="card mt-3">
+                    <div className="card-body">
+                        <img className='img-fluid' alt="img" src="https://img.freepik.com/free-psd/blank-wall-psd-japandi-living-room-interior_53876-109284.jpg" width="100%"/>
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#go" className="btn btn-primary">Go</a>
+                    </div>
+                    </div>
                 </div>
+
             </div>
-        </div>
-
-
-        <div className="col-md-6">
-            <div className="container card mt-4" style={{width: "35rem"}}>
-            <img className="card-img-top mt-3" src="https://images.pexels.com/photos/1669799/pexels-photo-1669799.jpeg?cs=srgb&dl=pexels-fotoaibe-1669799.jpg&fm=jpg" alt="Card images cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#go" className="btn btn-primary">Go</a>
-                </div>
-            </div>
-        </div>
-
-
-
     </div>
 </div>
 <div className="col-md-1"></div>
