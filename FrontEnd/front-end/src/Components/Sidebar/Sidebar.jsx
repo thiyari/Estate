37daf2cc1a76fca4439cef4b87ad70b46eb494c7
thiyari <manikanth578@gmail.com>
2024-07-24 11:36:42 +0530

    update

--- FrontEnd/front-end/src/Components/Sidebar/Sidebar.jsx
+++ FrontEnd/front-end/src/Components/Sidebar/Sidebar.jsx
@@ -12,7 +12,7 @@ import {
 const Sidebar = () => {
 
   return (
-      <CDBSidebar className="border" maxWidth="230px" textColor="#333" backgroundColor="#f8f9fb">
+      <CDBSidebar className="border" maxWidth="230px" textColor="#333" backgroundColor="#ffffff">
         <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
           <div className="container" style={{ display: 'flex', alignItems: 'center' }}>
             <img
