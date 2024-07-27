007bc87a6e434b55a5071f3f4cff31ad9aebe330
thiyari <manikanth578@gmail.com>
2024-07-27 22:43:40 +0530

    update

--- FrontEnd/front-end/src/Components/Search.jsx
+++ FrontEnd/front-end/src/Components/Search.jsx
@@ -5,6 +5,9 @@ import axios from 'axios'
 function Search(props) {
 
     const [loggedIn, setLoggedIn] = useState(false)
+    const [dataExists, setDataExists] = useState(false)
+    const [profiles, setProfiles] = useState([{}])
+
     const session = useCallback(async ()=>{
         await axios.get(`${process.env.REACT_APP_SERVER_URI}/api/session`)
         .then(res => {
@@ -18,38 +21,69 @@ function Search(props) {
         .catch(err => console.log(err))  
     },[props, loggedIn])
 
+
+    const records = useCallback(async()=>{
+        await axios.get(`${process.env.REACT_APP_SERVER_URI}/api`)
+            .then(res => {
+                let profiles_doc = res.data.records
+                if (!Object.keys(profiles_doc).length) { // Check for empty data in the response
+                    setDataExists(false)
+                } else {
+                    let profiles_list = []
+                    for (let i = 0; i < profiles_doc.length;  i++) {
+                        profiles_list.push(profiles_doc[i])
+                    }
+                    setProfiles(profiles_list)
+                    setDataExists(true)
+                }
+            })
+    },[]);
+
     axios.defaults.withCredentials = true;
     useEffect(()=>{
       session();
-    },[session]);
+      records();
+    },[session, records]);
 
 return(
-<>
+    <>
+{dataExists && (
+    
 <div className="container mt-4">
       <div className="row">
       <div className="col-md-1"></div>
           <div className="col-md-10">
               <div className ="table-responsive-md">
                   <table className ="table table-striped">
-                    <thead>
+                    <thead align="center">
                       <tr>
-                        <th>##</th>
-                        <th>##</th>
+                        <th>Property ID</th>
+                        <th>Property Type</th>
+                        <th>Area</th>
+                        <th>Phase</th>
+                        <th>Rooms</th>
+                        <th>Floor</th>
+                        <th>Location</th>
+                        <th>Currency</th>
+                        <th>Price</th>
                       </tr>
                     </thead>
-                    <tbody className="table-group-divider">
+                    <tbody className="table-group-divider" align="center">
+                    {profiles.map((profile, index)=>{ 
+                            return (<>
                       <tr>
-                        <td align="center"></td>
-                        <td align="center"></td>
+                        <td>{profile.propertyid}</td>
+                        <td>{profile.property}</td>
+                        <td>{profile.area}</td>
+                        <td>{profile.phase}</td>
+                        <td>{profile.rooms}</td>
+                        <td>{profile.floor}</td>
+                        <td>{profile.location}</td>
+                        <td>{profile.currency}</td>
+                        <td>{profile.price}</td>
                       </tr>
-                      <tr>
-                        <td align="center"></td>
-                        <td align="center"></td>
-                      </tr>
-                      <tr>
-                        <td align="center"></td>
-                        <td align="center"></td>
-                      </tr>
+                            </>)}
+                    )}
                     </tbody>
                   </table>
               </div>                       
@@ -57,7 +91,9 @@ return(
               <div className='col-md-1'></div>
               </div>
               </div>
-</>
+            
+            )}
+            </>
 )}
 
 export default Search;
