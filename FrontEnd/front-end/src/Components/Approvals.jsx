d69d0df55236bf6ace1c23f3a044a4c8740d0810
thiyari <manikanth578@gmail.com>
2024-08-02 18:52:28 +0530

    update

--- FrontEnd/front-end/src/Components/Approvals.jsx
+++ FrontEnd/front-end/src/Components/Approvals.jsx
@@ -1,10 +1,23 @@
 import axios from "axios";
 import React, { useState, useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import AdminSidebar from "./Sidebar/AdminSidebar";
 
 function Approvals(props) {
     const [loggedIn, setLoggedIn] = useState(false)
+    const { propertyId } = useParams()
+    const [Id, setId] = useState('')
+    const [propertyLocation, setPropertyLocation] = useState('')
+    const [propertyArea, setPropertyArea] = useState('')
+    const [propertyType, setPropertyType] = useState('')
+    const [phase, setPhase] = useState('')
+    const [rooms, setRooms] = useState('')
+    const [floor, setFloor] = useState('')
+    const [currency, setCurrency] = useState('')
+    const [price, setPrice] = useState('')
+    const [zip, setZip] = useState('')
+    const [propertyAddress, setPropertyAddress] = useState('')
+
     const navigate = useNavigate()
     const session = useCallback(async () =>{
         await axios.get(`${process.env.REACT_APP_SERVER_URI}/api/session`)
@@ -20,10 +33,42 @@ function Approvals(props) {
         .catch(err => console.log(err))
       },[props, loggedIn, navigate])
 
+      const property_id = useCallback(async ()=>{
+        await axios.get(`${process.env.REACT_APP_SERVER_URI}/api/approvals`)
+        .then(res => {
+            const record = res.data.records.filter((doc)=>(doc.propertyid === propertyId))
+            setId(record[0]._id)
+        })
+      },[propertyId]) 
+
+      const profile = useCallback(async () => {
+        await axios.get(`${process.env.REACT_APP_SERVER_URI}/api/profile/${Id}`)
+        .then(res => {
+          if(res.data.status){
+            const profile_doc = res.data.profile      
+            if (profile_doc.data[0].requests === 'Registered'){
+              setPropertyLocation(profile_doc.data[0].location)
+              setPropertyArea(profile_doc.data[0].area)
+              setPropertyType(profile_doc.data[0].property)
+              setPhase(profile_doc.data[0].phase)
+              setRooms(profile_doc.data[0].rooms)
+              setFloor(profile_doc.data[0].floor)
+              setCurrency(profile_doc.data[0].currency)
+              setPrice(profile_doc.data[0].price)
+              setZip(profile_doc.data[0].zip)
+              setPropertyAddress(profile_doc.data[0].address)
+              } 
+          } 
+        })
+        .catch(err => console.log(err))
+      },[Id])
+
       axios.defaults.withCredentials = true;
       useEffect(()=>{
         session();
-      },[session])
+        property_id();
+        profile();
+      },[session, property_id, profile])
 
 
     return(
@@ -45,10 +90,66 @@ function Approvals(props) {
             <div className="form-container">
                 <form>
                 <div className="card-body">
-                    
-                    
-                    
-                </div>       
+                <div className="col-md-12">
+                <div className='row'>
+                <div className="col-sm-2"></div>
+                <div className="col-sm-8">
+                    <table className="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">Particulars</th>
+                            <th scope="col">Details</th>
+                        </tr>
+                        </thead>
+                        <tbody className="table-group-divider">
+                        
+                        <tr>
+                            <td>Property Location</td>
+                            <td>{propertyLocation}</td>
+                        </tr>
+                        <tr>
+                            <td>Property Area</td>
+                            <td>{propertyArea}</td>
+                        </tr>
+                        <tr>
+                            <td>Property Type</td>
+                            <td>{propertyType}</td>
+                        </tr>
+                        <tr>
+                            <td>Phase</td>
+                            <td>{phase}</td>
+                        </tr>
+                        <tr>
+                            <td>Rooms</td>
+                            <td>{rooms}</td>
+                        </tr>
+                        <tr>
+                            <td>Floor</td>
+                            <td>{floor}</td>
+                        </tr>
+                        <tr>
+                            <td>Currency</td>
+                            <td>{currency}</td>
+                        </tr>
+                        <tr>
+                            <td>Price</td>
+                            <td>{price}</td>
+                        </tr>
+                        <tr>
+                            <td>Zip</td>
+                            <td>{zip}</td>
+                        </tr>
+                        <tr>
+                            <td>Property Address</td>
+                            <td>{propertyAddress}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="col-md-2"></div>
+                </div>  
+                </div>
+                </div>     
                 </form>
             </div>
             </div>
