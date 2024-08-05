7806760feb18b06a244a5995bc663b805bbc84a3
thiyari <manikanth578@gmail.com>
2024-08-05 18:59:16 +0530

    upgrade user sidebar

--- FrontEnd/front-end/src/Components/Sidebar/UserSidebar.jsx
+++ FrontEnd/front-end/src/Components/Sidebar/UserSidebar.jsx
@@ -1,43 +1,52 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { FaUser } from "react-icons/fa";
+import { BsBuildingsFill } from "react-icons/bs";
+import { RiLockPasswordFill } from "react-icons/ri";
 
-import {
-  CDBSidebar,
-  CDBSidebarHeader,
-  CDBSidebarMenuItem,
-  CDBSidebarContent,
-  CDBSidebarMenu,
-} from 'cdbreact';
+const UserSidebar = () => {
+  const [toggled, setToggled] = React.useState(false);
+  const [broken, setBroken] = React.useState(false);
+  const [collapsed, setCollapsed] = React.useState(false);
 
-const Sidebar = () => {
+
+ 
 
   return (
-      <CDBSidebar className="border" maxWidth="230px" textColor="#333" backgroundColor="#ffffff">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
-          <div className="container" style={{ display: 'flex', alignItems: 'center' }}>
-            <img
-              src={'https://seeklogo.com/images/B/butterfly-logo-0A00378822-seeklogo.com.png'}
-              alt=""
-              style={{ width: '30px' }}
-            />
-            <h6 className="ms-2">ESTATE ™</h6>
-          </div>
-        </CDBSidebarHeader>
-        <CDBSidebarContent>
-          <CDBSidebarMenu>
-            <NavLink reloadDocument exact="true" to="/Profile" className={(navData) => (navData.isActive ? "active-style" : 'none')}>
-              <CDBSidebarMenuItem icon="fa fa-user">Profile</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact="true" to="/ChangePassword" className={(navData) => (navData.isActive ? "active-style" : 'none')}>
-              <CDBSidebarMenuItem icon="fa-solid fa-gear">Change Password</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink reloadDocument exact="true" to="/Property" className={(navData) => (navData.isActive ? "active-style" : 'none')}>
-              <CDBSidebarMenuItem icon="fa-solid fa-building">My Property</CDBSidebarMenuItem>
-            </NavLink>
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
-      </CDBSidebar>
-  )
+    <div style={{ display: 'flex', height: '100%'}}>
+    <Sidebar 
+      collapsed={collapsed}
+      toggled={toggled}
+      onBackdropClick={() => setToggled(false)}
+      onBreakPoint={setBroken}
+      breakPoint="md"
+    className='border'>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%', backgroundColor: '#fff' }}>
+        <div style={{ flex: 1, marginBottom: '32px' }}>
+          <Menu>
+            <MenuItem icon={<button className="btn" style={{width:50}} onClick={() => setCollapsed(!collapsed)}><i className="fa fa-bars" /></button>}>Estate ™</MenuItem>
+              <MenuItem icon={<FaUser />} component={<Link to="/Profile" />}>Profile</MenuItem>
+              <MenuItem icon={<RiLockPasswordFill />} component={<Link to="/ChangePassword" />}>Change Password</MenuItem>
+              <MenuItem icon={<BsBuildingsFill />} component={<Link to="/Property" />}>My Property</MenuItem>
+          </Menu>          
+        </div>
+      </div>
+    </Sidebar>
+
+    <main>
+      <div style={{ padding: '16px 24px', color: '#44596e' }}>
+        <div style={{ marginBottom: '16px' }}>
+          {broken && (
+            <button className="sb-button" onClick={() => setToggled(!toggled)}>
+              <i className="fa fa-bars" />
+            </button>
+          )}
+        </div>
+      </div>
+    </main>
+  </div>
+)
 };
 
-export default Sidebar;
+export default UserSidebar;
