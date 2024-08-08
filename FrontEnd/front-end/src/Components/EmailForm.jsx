ae78e6cfc1db01e7b11baf02f260995dd396b250
thiyari <manikanth578@gmail.com>
2024-08-08 19:43:50 +0530

    Email Form Validation done

--- FrontEnd/front-end/src/Components/EmailForm.jsx
+++ FrontEnd/front-end/src/Components/EmailForm.jsx
@@ -4,11 +4,20 @@ import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 function EmailForm(props){
+
+  
+  const initialState = {
+    subject: "",
+    message: "",
+    subject_status: "",
+    messsage_status: ""
+  }
+
     const [loggedIn, setLoggedIn] = useState(false)
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
     const { email } = useParams()
-
+    const [formError, setFormError] = useState({...initialState})
     const navigate = useNavigate()
 
     const session = useCallback(async () =>{
@@ -30,8 +39,35 @@ function EmailForm(props){
         session();
       },[session])
 
+       // Initialize an object to track input errors
+      let inputError = {...initialState};
+
       const handleSubmit = async (e) => {
         e.preventDefault();
+
+          // Check if subject is empty
+          if(!subject){
+            setFormError({
+              ...inputError,
+              subject: "Please enter the Subject",
+              subject_status: "error"
+            })
+            return;
+          }
+
+          // Check if message is empty
+          if(!message){
+            setFormError({
+              ...inputError,
+              message: "Please type your message",
+              message_status: "error"
+            })
+            return;
+          }          
+
+          // Clear any previous errors
+          setFormError(inputError);
+
         try {
           await axios.post(`${process.env.REACT_APP_SERVER_URI}/api/send-email`, JSON.stringify({
             to: email,
@@ -94,8 +130,11 @@ function EmailForm(props){
             placeholder="Subject"
             value={subject}
             onChange={(e) => setSubject(e.target.value)}  
+            style={{borderColor: formError.subject_status !== "error" ?"":"red"}}
           />
           </div>
+          <p className="error-message">{formError.subject}</p>
+
           <div className="form-group">
           <div className="mb-3">
           <label htmlFor="message" className="form-label">Message</label>
@@ -105,10 +144,13 @@ function EmailForm(props){
             placeholder="Message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}  
+            style={{borderColor: formError.message_status !== "error" ?"":"red"}}
           >  
           </textarea>
           </div>
           </div>
+          <p className="error-message">{formError.message}</p>
+
           <div align="center">
           <button type="submit" className='btn btn-primary' style={{width: 150}}>Send Email</button>
           </div>
