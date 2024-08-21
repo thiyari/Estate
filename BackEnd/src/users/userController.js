b5d87fe3476e6bc0d3bd695c586492908a093bb4
thiyari <manikanth578@gmail.com>
2024-08-21 21:58:26 +0530

    update

--- BackEnd/src/users/userController.js
+++ BackEnd/src/users/userController.js
@@ -101,14 +101,11 @@ var loginUserControllerFn = async(req,res)=>
 var logoutUserControllerFn = async(req,res)=>
     {
         if(session.username){
-            /*
             session.username = ""
             session.password = ""
             session.isLoggedIn = false;
             session.id = ""
             session.logstatus = ""
-            */
-            store.clear();
             res.clearCookie('connect.sid');
             return res.json({valid: true})
         } else {
