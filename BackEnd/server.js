4898a872fcfb1f60749d28ba0d4012652ae4dc72
thiyari <manikanth578@gmail.com>
2024-08-25 17:11:04 +0530

    update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -14,12 +14,14 @@ const MemoryStore = require('memorystore')(session)
 
 dotenv.config({path: path.resolve(__dirname, 'config.env')})
 
-app.use(cors(
-    {   origin: [process.env.REACT_APP_CLIENT_LOCAL_URI, process.env.REACT_APP_CLIENT_URI],
-        methods: ['POST','GET','PUT','DELETE'],
-        credentials:true,            //access-control-allow-credentials:true
-        optionSuccessStatus:200,}
-));
+        // Do not block requests while in development
+        app.UseCors(options =>
+            {
+                options.AllowAnyOrigin();
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+            
 app.use(express.json({limit:"10mb"}))
 
 app.use(cookieParser());
