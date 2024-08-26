0425bc7d72598d8b6181ce36eb8fa3acda94ec58
thiyari <manikanth578@gmail.com>
2024-08-26 17:33:00 +0530

    update

--- FrontEnd/front-end/src/properties.js
+++ FrontEnd/front-end/src/properties.js
@@ -1,4 +1,5 @@
 export const properties = {
     REACT_APP_SERVER_LOCAL_URI: "http://localhost:8000",
-    REACT_APP_SERVER_URI: "https://estate-server-x414.onrender.com"
+    REACT_APP_SERVER_URI: "https://estate-production-edb7.up.railway.app"
+    //REACT_APP_SERVER_URI: "https://estate-server-x414.onrender.com"
 };
