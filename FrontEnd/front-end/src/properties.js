3ef63bd5bb217787b900b7a64f014f43cf8b3ca4
thiyari <manikanth578@gmail.com>
2024-08-27 20:49:32 +0530

    update

--- FrontEnd/front-end/src/properties.js
+++ FrontEnd/front-end/src/properties.js
@@ -1,4 +1,5 @@
 export const properties = {
     REACT_APP_SERVER_LOCAL_URI: "http://localhost:8000",
-    REACT_APP_SERVER_URI: "https://estate-server-x414.onrender.com"
+    //REACT_APP_SERVER_URI: "https://estate-server-x414.onrender.com"
+    REACT_APP_SERVER_URI: "https://estate-server-five.vercel.app/"
 };
