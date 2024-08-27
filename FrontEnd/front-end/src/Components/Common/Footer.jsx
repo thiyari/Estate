cbb0aaac582241c548ae6c49c9e79f8b2343ed5d
thiyari <manikanth578@gmail.com>
2024-08-27 16:46:00 +0530

    update

--- FrontEnd/front-end/src/Components/Common/Footer.jsx
+++ FrontEnd/front-end/src/Components/Common/Footer.jsx
@@ -9,8 +9,8 @@ function Footer(){
                 <div className='container p-4'>
                     <div className='row'>
                         <div className='col-sm-6'>
-                            <h5>Our Motive</h5>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <h5>Kind Note</h5>
+                            <p>The information provided above in this website is only meant for visual understanding purposes. The data incorporated is not really meant for selling, purchasing or leasing the properties. This site is under development phase.</p>
                         </div>
                         <div className='col-sm-2'>
                             <h5>Navigation</h5>
