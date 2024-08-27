79c200f4e25453c4a530e798524dff0b703a6003
thiyari <manikanth578@gmail.com>
2024-08-27 21:09:23 +0530

    update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -23,7 +23,7 @@ app.use(cors(
 */
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', 'https://estate-client-ruby.vercel.app')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
