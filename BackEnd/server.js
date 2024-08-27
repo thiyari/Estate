968a66fe357d6ed7e7c415ac4f1a9916dfd515ce
thiyari <manikanth578@gmail.com>
2024-08-27 21:23:13 +0530

    update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -110,3 +110,20 @@ app.post("/send-bulk-emails",upload.array("files"),async(req,res)=>{
 connectDB();
 
 app.use(routes);
+module.exports = (req, res) => {
+    //set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', 'https://estate-client-ruby.vercel.app');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+ //Preflight CORS handler
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+
+}
