365fe106acb9fd5dcc783956b6fee262cea34d5c
thiyari <manikanth578@gmail.com>
2024-08-28 01:03:12 +0530

    update choreo server uri

--- FrontEnd/front-end/src/properties.js
+++ FrontEnd/front-end/src/properties.js
@@ -1,4 +1,4 @@
 export const properties = {
     REACT_APP_SERVER_LOCAL_URI: "http://localhost:8000",
-    REACT_APP_SERVER_URI: "https://estate-server-x414.onrender.com"
+    REACT_APP_SERVER_URI: "https://433bb6f0-4bb6-4242-8a00-8e7d14d4a66e.e1-us-cdp-2.choreoapps.dev"
 };
