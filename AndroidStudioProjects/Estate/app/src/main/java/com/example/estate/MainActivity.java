d9d1b911d4fc8d59eb0319ece41b6913d089ca1a
thiyari <manikanth578@gmail.com>
2024-09-01 20:38:25 +0530

    andriod release

---
+++ AndroidStudioProjects/Estate/app/src/main/java/com/example/estate/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.estate;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://estate-client-ruby.vercel.app/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    private class MywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+
+}
